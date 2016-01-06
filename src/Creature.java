b4acd4b1a6bdbcbe9d53ec6703e0066abe71ba34
Kemacian <loic.toro@alumni.univ-avignon.fr>
2016-01-06 17:36:28 +0100

    [UPDATE] JAVADOC

--- src/Creature.java
+++ src/Creature.java
@@ -4,21 +4,21 @@
  *
  */
 public class Creature {
-	/**  */
+	/** Le nom du tamagochi. */
 	private String name;
-	/**	 */
+	/**	La race du tamagochi identifiée avec un ID */
     public int raceId;
-    /**	 */
+    /**	La jauge de faim du tamagochi */
     private int hunger;
-    /**	 */
+    /**	La jauge de moral du tamagochi */
     private int morale;
-    /**	 */
+    /**	La jauge de concentration du tamagochi */
     private int focus;
-    /**	 */
+    /**	Le statut du tamagochi */
     private int status;
     
     /**	 
-     * 
+     * Constructeur vide d'un objet Creature
      */
     public Creature()
     {
@@ -36,11 +36,12 @@ public class Creature {
     }
     
     /**
+     * Second constructeur d'un objet Creature
      * 
      * @param choosedName
-     * 
+     * 		Le nom du tamagochi
      * @param choosedRace
-     * 
+     * 		L'id de la race du tamagochi
      */
     public Creature(String choosedName, int choosedRace)
     {
@@ -56,45 +57,49 @@ public class Creature {
     }
     
     /**
+     * Met à jour la jauge de faim.
      * 
      * @param newHunger
-     * 
+     * 		La valeur qui modifie la jauge.
      */
 	public void modifyHunger(int newHunger)
 	{
-		this.hunger = this.hunger + newHunger;
+		this.hunger += newHunger;
 	    if (this.hunger > 100) this.hunger = 100;
 	    if (this.hunger < 0) this.hunger = 0;
 	}
 	
 	/**
+	 * Met à jour la jauge de moral.
 	 * 
 	 * @param newMorale
-	 * 
+	 * 		La valeur qui modifie la jauge.
 	 */
 	public void modifyMorale(int newMorale)
 	{
-		this.morale = this.morale + newMorale;
+		this.morale += newMorale;
 	    if (this.morale > 100) this.morale = 100;
 	    if (this.morale < 0) this.morale = 0;
 	}
 	
 	/**
+	 * Met à jour la jauge de concentration.
 	 * 
 	 * @param newFocus
-	 * 
+	 * 		La valeur qui modifie la jauge.
 	 */
 	public void modifyFocus(int newFocus)
 	{
-		this.focus = this.focus + newFocus;
+		this.focus += newFocus;
 	    if (this.focus > 100) this.focus = 100;
 	    if (this.focus < 0) this.focus = 0;
 	}
 	
 	/**
+	 * Remplace l'ID de la race du tamagochi.
 	 * 
 	 * @param raceId
-	 * 
+	 * 		Le nouvel ID.
 	 */
 	public void setRaceId(int raceId)
 	{
@@ -102,9 +107,10 @@ public class Creature {
 		return;
 	}
 	/**
+	 * Remplace le statut du tamagochi.
 	 * 
 	 * @param newStatus
-	 * 
+	 * 		Le nouveau tamagochi.
 	 */
 	public void setStatus(int newStatus)
 	{
@@ -113,9 +119,10 @@ public class Creature {
 	}
 	
 	/**
+	 * Change le nom du tamagochi.
 	 * 
 	 * @param name
-	 * 
+	 * 		
 	 */
 	public void setName(String name)
 	{
